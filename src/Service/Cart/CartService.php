3f7d4746eedca735d3f1202dd434936a540d9f17
Sylvain Guerrin <sguerrin549@gmail.com>
2020-02-04 11:38:53 +0100

    ajout dans le cartService des fonctions remove,less,getFullCart

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -2,15 +2,18 @@
 
 namespace App\Service\Cart;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CartService {
 
     protected $session;
+    protected $productRepository;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, ProductRepository $productRepository)
     {
         $this->session = $session;
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -31,14 +34,43 @@ class CartService {
     /**
      * @param int $id
      */
+    public function less(int $id): void
+    {
+        $panier = $this->session->get('panier', []);
+        if (!empty($panier[$id])) {
+            if ($panier[$id] === 1) {
+                unset($panier[$id]);
+            }else{
+                $panier[$id]--;
+            }
+            $this->session->set('panier', $panier);
+        }
+
+    }
+
+    /**
+     * @param int $id
+     */
     public function remove(int $id): void
     {
+        $panier = $this->session->get('panier', []);
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+        $this->session->set('panier', $panier);
 
     }
 
     public function getFullCart(): array
     {
-
+        $panier = $this->session->get('panier', []);
+        $panierWithData = [];
+        foreach ($panier as $id => $quantity){
+            $panierWithData[] = [
+                'product' => $this->productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
     }
 
     public function getTotal(): float
